5984a87d5454b7e8dba3d28ab9fd6d8a61f4539a
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2015-04-04 21:02:56 -0500

    Add new design; Successfully implement data load from json file

--- js/default.js
+++ js/default.js
@@ -1,186 +1,76 @@
 $(document).ready(onReady);
 
-var currentSubreddit;
-
 function onReady()
 {
-    //  Add link functionality
-    $('.link_start').click(function(){
-        setNav('#nav_start');
-        showPage('#page_start');
-    });
-    $('.link_home').click(function(){
-        setNav('#nav_home');
-        showPage('#page_home');
-    });
-
-    //  Add tab functionality
-    $('.tab_popular').click(function(){
-        setTab('#tab_popular');
-        showSection('#section_popular');
-    });
-    $('.tab_saved').click(function(){
-        setTab('#tab_saved');
-        showSection('#section_saved');
-    });
-    $('.tab_friends').click(function(){
-        setTab('#tab_friends');
-        showSection('#section_friends');
-    });
-    $('.tab_settings').click(function(){
-        setTab('#tab_settings');
-        showSection('#section_settings');
-    });
-
-    //  Show home page
-    $('#page_start').addClass('activePage');
-    $('#page_start').fadeIn();
-
-    //  Set default tab
-    $('#section_popular').addClass('activeSection');
-    $('#section_popular').fadeIn();
-
+    loadPage("home");
 }
 
-function setNav(nav)
+function loadPage(pageName)
 {
-
-    $('.toggle-topbar > a').trigger( "click" );
-    if($(nav).hasClass('active')) return;
-
-    $('nav > section > ul > li').removeClass('active');
-    $(nav).addClass('active');
-
+    switch(pageName)
+    {
+        case "home":
+            loadPageHelper("html/home.html",homeReady,pageName);
+            break;
+        default: 
+            loadPageHelper("html/home.html", homeReady,pageName);
+            break;
+    }
 }
 
-function setTab(tab)
+function loadPageHelper(htmlFile, func, pageName)
 {
-    if($(tab).hasClass('active')) return;
-
-    $('.tab').removeClass('active');
-    $(tab).addClass('active');
-}
-
-function showPage(page)
-{
-    if($(page).hasClass('activePage')) return;
-
-    $('.activePage').slideUp(400, function()
+    $('#page_active').fadeOut(function() {
+        $('#page_active').load(htmlFile,function() 
         {
-            $(page).slideDown();
-            $('.activePage').removeClass('activePage');
-            $(page).addClass('activePage');
-        }
-    );
-}
-
-function showSection(section)
-{
-    if($(section).hasClass('activeSection')) return;
-
-    $('.activeSection').slideUp(400, function()
-        {
-            $(section).slideDown();
-            $('.activeSection').removeClass('activeSection');
-            $(section).addClass('activeSection');
-        }
-    );
-}
-
-function loadSubreddit(subreddit)
-{
-    $('.reddit_content').html("");
-
-    currentSubreddit = subreddit;
-    var url = "http://api.reddit.com/r/" + subreddit;
-
-    $.getJSON(url, function(data){
-
-        $.each( data.data.children, function( key, val ) {
-            var is_self = val.data.is_self;
-            var title = val.data.title;
-            var author = val.data.author;
-            var url = val.data.url;
-            var domain = val.data.domain;
-            var selftext = val.data.selftext;
-
-            if(is_self)
-            {
-                $('.reddit_content').append(''+
-                '<div class="panel" style="padding: 0px;">'+
-                    '<div class="row">' +
-                        '<div class="small-12 columns">' +
-                            '<div style="padding: 1.25rem;">' +
-                                '<h3>'+title+'</h3>' +
-                                '<p style="color: gray; margin-bottom: 0px;">'+author+'</p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                    '<hr>' +
-                    '<div class="row">' +
-                        '<div class="small-12 columns">' +
-                            '<div style="padding: 1.25rem;">' +
-                                '<p>'+selftext+'</p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>'
-                );
-            }
-            else if(domain.indexOf("i.imgur") > -1)
-            {
-                $('.reddit_content').append(''+
-                '<div class="panel" style="padding: 0px;">'+
-                    '<div class="row">' +
-                        '<div class="small-12 columns">' +
-                            '<div style="padding: 1.25rem;">' +
-                                '<h3>'+title+'</h3>' +
-                                '<p style="color: gray; margin-bottom: 0px;">'+author+'</p>' +
-                            '</div>' +
-                            '<a href='+url+' target="_blank" >' +
-                                '<div style="width: 100%; height: 250px;">' +
-                                    '<div style="height: 100%; background-size: cover; -moz-background-size: cover;' +
-                                    ' -webkit-background-size: cover; background-position: center; background-image: url('+url+');">' +
-                                    '</div>' +
-                                '</div>' +
-                            '</a>' + 
-                        '</div>' +
-                    '</div>' +
-                '</div>'
-                );
-            }
-            else
-            {
-                $('.reddit_content').append(''+
-                '<div class="panel" style="padding: 0px;">'+
-                    '<div class="row">' +
-                        '<div class="small-12 columns">' +
-                            '<div style="padding: 1.25rem;">' +
-                                '<h3>'+title+'</h3>' +
-                                '<p style="color: gray; margin-bottom: 0px;">'+author+'</p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                    '<hr>' +
-                    '<div class="row">' +
-                        '<div class="small-12 columns">' +
-                            '<div style="padding: 1.25rem;">' +
-                                '<a href='+url+' target="_blank">'+url+'</a>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>'
-                );
-            }
+            $('#page_active').fadeIn(func);
         });
+    });
+}
+
+function homeReady()
+{
+    $('.home-tabs a.tab').click(tabClicked);
+
+    $.getJSON( "data/data.json",function(data){
+
+        var item;
+
+        for (var i = 0; i < data.popular_lists.length; i++) 
+        {
+            item = data.popular_lists[i];
+
+            if(i==data.popular_lists.length-1) 
+                addPanel(item.title,item.username, true);
+            else 
+                addPanel(item.title,item.username, false);
+        };
+
+        $('.content-panel').slideDown();
 
     });
+}
 
-    $('.reddit_image').click(function(){
-        console.log("URL");
-        /*
-        url = $(this).attr('data-url');
-        $('#modal_img').attr('src',url);
-        */
-    });
+function tabClicked()
+{
+    $('.home-tabs a.tab.active').removeClass('active');
+    $(this).addClass('active');
+}
+
+function addPanel(title, username, margin)
+{
+    var style="";
+    if(margin) style=' style="margin-bottom:0px;"';
+
+
+    $('.content-panel').append(
+    '<div class="row">'+
+        '<div class="small-12 columns">'+
+            '<div class="panel"'+style+'>'+
+                '<h5>'+title+'</h5>'+
+                '<p>'+username+'</p>'+
+            '</div>'+
+        '</div>'+
+    '</div>'
+    );
 }
