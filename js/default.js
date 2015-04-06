a44e65cc393c0f57bdb8cb67c87fe455c70b3171
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2015-04-05 23:56:17 -0500

    Implement dominant searchbar; Add simple login modal and alert demo; Give tabs simple functionality; Trigger search on keyup for searchbar;

--- js/default.js
+++ js/default.js
@@ -1,8 +1,13 @@
 $(document).ready(onReady);
 
+var loggedIn;
+
 function onReady()
 {
+    loggedIn = false;
     loadPage("home");
+    $('.link_login').click(promptLogin);
+    $('#btn_login').click(login);
 }
 
 function loadPage(pageName)
@@ -31,44 +36,161 @@ function loadPageHelper(htmlFile, func, pageName)
 function homeReady()
 {
     $('.home-tabs a.tab').click(tabClicked);
-
-    $.getJSON( "data/data.json",function(data){
-
-        var item;
-
-        for (var i = 0; i < data.popular_lists.length; i++) 
-        {
-            item = data.popular_lists[i];
-
-            if(i==data.popular_lists.length-1) 
-                addPanel(item.title,item.username, true);
-            else 
-                addPanel(item.title,item.username, false);
-        };
-
-        $('.content-panel').slideDown();
-
-    });
+    $('.search-column').keyup(searchBarClicked);
+    $('#tab_popular').click();
 }
 
 function tabClicked()
 {
-    $('.home-tabs a.tab.active').removeClass('active');
-    $(this).addClass('active');
+    var tab = $(this).attr('id');
+
+    switch(tab)
+    {
+        case 'tab_popular':
+            if($(this).hasClass('active')) return;
+            switchToTab(tab);
+            getPlaylists('popular');
+            break;
+        case 'tab_saved':
+            if($(this).hasClass('active')) return;
+            if(loggedIn)
+            {
+                switchToTab(tab);
+                getPlaylists('saved');
+            }
+            else promptLogin();
+            break;
+        case 'tab_friends':
+            if($(this).hasClass('active')) return;
+            if(loggedIn)
+            {
+                switchToTab(tab);
+                getPlaylists('friends');
+            }
+            else promptLogin();
+            break;
+        case 'tab_search':
+            switchToTab(tab);
+            search($('#create_query').val() + " " + $('#find_query').val());
+            return;  
+    }
 }
 
-function addPanel(title, username, margin)
+function search(query)
+{
+    $('.content-panel').slideUp(function(){
+        $('.content-panel').html("");
+        if(query != " ")
+        {
+            addPanel(query,"Artist",true);
+            $('.content-panel').slideDown();
+        }
+    });
+}
+
+function promptLogin()
+{
+    $('#loginModal').foundation('reveal','open');
+}
+
+function login()
+{
+    $('#loginModal').foundation('reveal', 'close');
+    loggedIn = true;
+    $('#loginAlertSuccess').slideDown();
+    $('#loginAlertSuccess .close').click(closeLoginAlert);
+}
+
+function closeLoginAlert()
+{
+    $('#loginAlertSuccess').slideUp();
+}
+
+function switchToTab(tab)
+{    
+    if($('#'+tab).hasClass('active')) return;
+    $('.home-tabs a.tab.active').removeClass('active');
+    $('#'+tab).addClass('active');
+}
+
+var lastClickedTab;
+
+function getPlaylists(src)
+{
+    lastClickedTab = src;
+
+    $('.content-panel').slideUp(function(){
+        $('.content-panel').html("");
+
+        $.getJSON( "data/data2.json",function(data){
+
+            var item;
+            var array;
+            console.log(data);
+
+            switch(lastClickedTab)
+            {
+                case 'popular':
+                    array = data.popular; break;
+                case 'saved':
+                    array = data.saved; break;
+                case 'friends':
+                    array = data.friends; break;
+                default:
+                    alert("uhoh");return;
+            }
+
+            for (var i = 0; i < array.length; i++) 
+            {
+                item = array[i];
+
+                if(i==array.length-1) 
+                    addPanel(item.title,item.username, true);
+                else 
+                    addPanel(item.title,item.username, false);
+            };
+
+            $('.content-panel').slideDown();
+
+        });
+    });
+}
+
+function searchBarClicked()
+{
+    if(!$(this).hasClass('medium-9'))
+    {
+        //  If first click
+        if($(this).hasClass('medium-6'))
+        {
+            $('.search-column').removeClass('medium-6');
+            $('.search-column').addClass('medium-3');
+
+            $(this).removeClass('medium-3');
+            $(this).addClass('medium-9');
+        }
+        else
+        {
+            $('.search-column').toggleClass('medium-3');
+            $('.search-column').toggleClass('medium-9');
+        }
+    }
+
+    $('#tab_search').click();
+}
+
+function addPanel(title, username, lastPanel)
 {
     var style="";
-    if(margin) style=' style="margin-bottom:0px;"';
+    if(lastPanel) style=' style="margin-bottom:0px;"';
 
 
     $('.content-panel').append(
     '<div class="row">'+
         '<div class="small-12 columns">'+
             '<div class="panel"'+style+'>'+
-                '<h5>'+title+'</h5>'+
-                '<p>'+username+'</p>'+
+                '<h4>'+title+'</h4>'+
+                '<h6>'+username+'</h6>'+
             '</div>'+
         '</div>'+
     '</div>'
