8ab5ee39ed1291808080420a881ce22ac19cd885
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2015-05-04 00:23:21 -0500

    Add simple search and playlist page

--- js/default.js
+++ js/default.js
@@ -5,22 +5,24 @@ var loggedIn;
 function onReady()
 {
     loggedIn = false;
-    loadPage("home");
+    $('.link_home').click(loadPage);
     $('.link_login').click(promptLogin);
     $('.link_logout').click(promptLogout);
+    $('#btn_create').click(login);
     $('#btn_login').click(login);
     $('#btn_logout').click(logout);
+    loadPage("home");
 }
 
 function loadPage(pageName)
 {
     switch(pageName)
     {
-        case "home":
-            loadPageHelper("html/home.html",homeReady,pageName);
+        case "play":
+            loadPageHelper("html/play.html?version=3",playReady,pageName);
             break;
         default: 
-            loadPageHelper("html/home.html", homeReady,pageName);
+            loadPageHelper("html/home.html?version=5", homeReady,pageName);
             break;
     }
 }
@@ -37,11 +39,22 @@ function loadPageHelper(htmlFile, func, pageName)
 
 function homeReady()
 {
-    $('.home-tabs a.tab').click(tabClicked);
+    $('.home-tabs a').click(tabClicked);
     $('.search-column').keyup(doSearch);
     $('.search-column').click(expandSearchbar);
     $('#tab_popular').click();
 }
+
+function playReady()
+{
+    //$(document).foundation('slider', 'reflow');
+
+
+    $('#btn_play').click(togglePlayPause);
+    $('#btn_save').click(toggleSave);
+}
+
+//  HOME  PAGE FUNCTIONS
 
 function tabClicked()
 {
@@ -54,6 +67,7 @@ function tabClicked()
             switchToTab(tab);
             getPlaylists('popular');
             break;
+
         case 'tab_saved':
             if($(this).hasClass('active')) return;
             if(loggedIn)
@@ -63,6 +77,7 @@ function tabClicked()
             }
             else promptLogin();
             break;
+
         case 'tab_friends':
             if($(this).hasClass('active')) return;
             if(loggedIn)
@@ -72,22 +87,113 @@ function tabClicked()
             }
             else promptLogin();
             break;
+
         case 'tab_search':
             switchToTab(tab);
-            search($('#create_query').val() + " " + $('#find_query').val());
+            if(searchbarId=="div_find")
+                searchPlaylists($('#find_query').val());
+            else searchSongs($('#create_query').val());
             return;  
     }
 }
 
-function search(query)
+var MAX_SONGS = 3;
+var MAX_ALBUMS = 2;
+
+function searchSongs(query)
 {
     $('.content-panel').slideUp(function(){
-        $('.content-panel').html("");
-        if(query != " ")
-        {
-            addPanel(query,"Artist",true);
-            $('.content-panel').slideDown();
-        }
+        $.getJSON( "data/music.json",function(data){
+
+            $('.content-panel').html("");
+            if(query != "")
+            {
+                var songs = data.songs;
+                var num_songs = 0;
+                var num_albums = 0;
+                var albums = [];
+
+
+                addLabel("SONGS");
+
+                for(var i = 0; i < songs.length; i++)
+                {
+                    if(songs[i].title.toLowerCase().indexOf(query.toLowerCase()) != -1 
+                        && num_songs < MAX_SONGS)
+                    {
+                        addPanel(songs[i].title, songs[i].artist, false);
+                        num_songs++;
+                    }
+                }
+
+                if(num_songs == 0)
+                    addLabel("No songs found.");
+
+                addLabel("");
+                addLabel("ALBUMS");
+
+                for(var i = 0; i < songs.length; i++)
+                {
+                    if((songs[i].album.toLowerCase().indexOf(query.toLowerCase()) != -1 ||
+                        songs[i].artist.toLowerCase().indexOf(query.toLowerCase()) != -1) 
+                        && num_albums < MAX_ALBUMS)
+                    {
+                        if(num_albums == 0 || (num_albums > 1 && albums[i-1] != songs[i].album))
+                        {
+                            addPanel(songs[i].album, songs[i].artist, false);
+                            albums[num_albums] = songs[i].album;
+                            num_albums++;
+                        }
+                    }
+                }
+
+                if(num_albums == 0)
+                    addLabel("No albums found.");
+
+                $('.content-panel').slideDown();
+            }
+        });
+    });
+}
+
+var MAX_LISTS = 5;
+
+function searchPlaylists(query)
+{
+    $('.content-panel').slideUp(function(){
+        $.getJSON( "data/music.json",function(data){
+
+            $('.content-panel').html("");
+            if(query != "")
+            {
+                var playlists = data.popular;
+                var num_lists = 0;
+                var lists = [];
+
+
+                addLabel("PLAYLISTS");
+
+                for(var i = 0; i < playlists.length; i++)
+                {
+                    if((playlists[i].title.toLowerCase().indexOf(query.toLowerCase()) != -1 ||
+                        playlists[i].username.toLowerCase().indexOf(query.toLowerCase()) != -1) 
+                        && num_lists < MAX_LISTS)
+                    {
+                        if(num_lists == 0 || (num_lists > 1 && lists[i-1] != playlists[i].title))
+                        {
+                            addPanel(playlists[i].title, playlists[i].username, false);
+                            lists[num_lists] = playlists[i].title;
+                            num_lists++;
+                        }
+                    }
+                }
+
+                if(num_lists == 0)
+                    addLabel("No playlists found.");
+
+                $('.content-panel').slideDown();
+            }
+        });
     });
 }
 
@@ -122,7 +228,7 @@ function login()
 
 function getUsername()
 {
-    return "Maria";
+    return $('#username').val();
 }
 
 function logout()
@@ -149,7 +255,7 @@ function closeAlert()
 function switchToTab(tab)
 {    
     if($('#'+tab).hasClass('active')) return;
-    $('.home-tabs a.tab.active').removeClass('active');
+    $('.home-tabs a.active').removeClass('active');
     $('#'+tab).addClass('active');
 }
 
@@ -162,7 +268,7 @@ function getPlaylists(src)
     $('.content-panel').slideUp(function(){
         $('.content-panel').html("");
 
-        $.getJSON( "data/data2.json",function(data){
+        $.getJSON( "data/music.json",function(data){
 
             var item;
             var array;
@@ -201,8 +307,12 @@ function doSearch()
     $('#tab_search').click();
 }
 
+var searchbarId;
+
 function expandSearchbar()
 {
+    searchbarId = $(this).attr('id');
+
     if(!$(this).hasClass('medium-9'))
     {
         //  If first click
@@ -225,17 +335,57 @@ function expandSearchbar()
 function addPanel(title, username, lastPanel)
 {
     var style="";
-    if(lastPanel) style=' style="margin-bottom:0px;"';
-
+    if(lastPanel) style = ' style="margin-bottom:0px;"';
 
     $('.content-panel').append(
-    '<div class="row">'+
-        '<div class="small-12 columns">'+
-            '<div class="panel"'+style+'>'+
-                '<h4>'+title+'</h4>'+
-                '<h6>'+username+'</h6>'+
+    '<a class="link-panel">'+
+        '<div class="row">'+
+            '<div class="small-12 columns">'+
+                '<div class="panel"'+style+'>'+
+                    '<h4>'+title+'</h4>'+
+                    '<h6>'+username+'</h6>'+
+                '</div>'+
             '</div>'+
         '</div>'+
-    '</div>'
+    '</a>'
     );
+
+    $('.link-panel').click(panelClicked);
+}
+
+function addLabel(str)
+{
+    $('.content-panel').append(
+        '<div class="row">'+
+          '<div class="small-12 columns">'+
+            '<h4 style="color: white;">'+str+'</h4>'+
+          '</div>'+
+        '</div>'
+    );
+}
+
+
+function panelClicked()
+{
+    loadPage("play");
+}
+
+
+//  PLAY PAGE STUFF
+
+function togglePlayPause()
+{
+    $("#btn_play > i").toggleClass("fi-play");
+    $("#btn_play > i").toggleClass("fi-pause");
+}
+
+function toggleSave()
+{
+    $('#btn_save').toggleClass("success");
+    promptReplace();
+}
+
+function promptReplace()
+{
+    $('#saveModal').foundation('reveal','open');
 }
